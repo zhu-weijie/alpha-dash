6a8cbafe486d01f26a6e29df7033ff75c7f91b4c
zhuwj <byzhubaiyuan@gmail.com>
2025-06-05 14:25:46 +0800

    Create User API Endpoint

---
+++ backend/app/api/endpoints/users.py
@@ -0,0 +1,25 @@
+# app/api/endpoints/users.py
+from fastapi import APIRouter, Depends, HTTPException, status
+from sqlalchemy.orm import Session
+from app import crud, schemas, models # noqa
+from app.db.session import get_db
+
+router = APIRouter()
+
+@router.post("/", response_model=schemas.User, status_code=status.HTTP_201_CREATED)
+def create_new_user(
+    *,
+    db: Session = Depends(get_db),
+    user_in: schemas.UserCreate
+):
+    """
+    Create new user.
+    """
+    user = crud.get_user_by_email(db, email=user_in.email)
+    if user:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="The user with this email already exists in the system.",
+        )
+    new_user = crud.create_user(db=db, user_in=user_in)
+    return new_user
