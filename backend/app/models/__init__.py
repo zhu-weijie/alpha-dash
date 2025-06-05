6a63b69a0a0ecdac3729a800f7cf08d4e1e02bc9
zhuwj <byzhubaiyuan@gmail.com>
2025-06-05 11:23:02 +0800

    Import all models

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,4 +1,4 @@
 # app/models/__init__.py
-from .user import User
-from .asset import Asset, AssetType
-from .portfolio_holding import PortfolioHolding
+from .user import User # noqa
+from .asset import Asset, AssetType # noqa
+from .portfolio_holding import PortfolioHolding # noqa
