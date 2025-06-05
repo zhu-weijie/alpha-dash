8190ee14d8ea7c90ec3855e30ce961c386da6136
zhuwj <byzhubaiyuan@gmail.com>
2025-06-05 20:25:35 +0800

    Implement CRUD Operations for Assets

--- backend/app/crud/__init__.py
+++ backend/app/crud/__init__.py
@@ -1,2 +1,10 @@
 # app/crud/__init__.py
 from .crud_user import create_user, get_user_by_email # noqa
+from .crud_asset import (
+    create_asset, 
+    get_asset, 
+    get_asset_by_symbol, 
+    get_assets, 
+    update_asset, 
+    remove_asset
+) # noqa
