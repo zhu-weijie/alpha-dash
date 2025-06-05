38c4415729851987607dabe19c06f29e8c033407
zhuwj <byzhubaiyuan@gmail.com>
2025-06-05 08:30:33 +0800

    Simplify App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,23 +1,13 @@
+// frontend/src/App.tsx
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>alpha-dash Frontend</h1>
+        <p>React application is running!</p>
       </header>
     </div>
   );
