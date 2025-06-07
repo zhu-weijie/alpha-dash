204b67702f3f18852d989ec49eac6dcaf2b897de
zhuwj <byzhubaiyuan@gmail.com>
2025-06-07 15:46:35 +0800

    Define marketData interface

---
+++ frontend/src/types/marketData.ts
@@ -0,0 +1,9 @@
+// src/types/marketData.ts
+export interface HistoricalPricePoint {
+    date: string;
+    open: number;
+    high: number;
+    low: number;
+    close: number;
+    volume: number;
+}
